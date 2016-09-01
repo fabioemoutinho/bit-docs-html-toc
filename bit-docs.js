b9aceaebb03dab3901c19919d9bd91cd5f41d274
Matthew Phillips <matthew@matthewphillips.info>
2016-09-01 15:44:45 -0400

    Initial commit

---
+++ bit-docs.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function(bitDocs){
+    var pkg = require("./package.json");
+    var dependencies = {};
+    dependencies[pkg.name] = pkg.version;
+
+    bitDocs.register("html", {
+        dependencies: dependencies
+    });
+}
