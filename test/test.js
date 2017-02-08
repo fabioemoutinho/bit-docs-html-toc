8172d94210efe5311ec18503533d3901faa664e4
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-02-08 14:56:50 -0700

    Allow setting the headings container selector
    Closes #13

--- test/test.js
+++ test/test.js
@@ -1,2 +1,4 @@
 require("./make-tree-test");
 require("./toc-control-test");
+require("./toc-container-control-test");
+
--- toc.js
+++ toc.js
@@ -1,35 +1,9 @@
-var Control = require("can-control");
-var TableOfContents = require("./toc-control");
+var TOCContainer =  require("./toc-container-control");
 
-var TOCContainer = Control.extend({
-	init: function(el) {
-		el.style.display = "none";
+var el = document.getElementsByClassName("on-this-page-container");
 
-		var depth = this.getOutlineDepth();
-		var tagName = this.getOutlineTagName();
-		var toc = document.createElement(tagName);
-
-		toc.className = "on-this-page";
-		el.appendChild(toc);
-
-		new TableOfContents(toc, {
-			depth: depth,
-			tagName: tagName,
-			headingsContainerSelector: "article"
-		});
-	},
-
-	getOutlineTagName: function() {
-		var outline = window.docObject.outline || {};
-		return (outline.tag === "ol") ? "ol" : "ul";
-	},
-
-	getOutlineDepth: function() {
-		var depth = window.docObject.outline && window.docObject.outline.depth;
-		return (typeof depth === "number" ? Math.min(depth, 6) : 1);
-	}
-});
-
-new TOCContainer(
-	document.getElementsByClassName("on-this-page-container")[0]
-);
+if (el.length) {
+	new TOCContainer(el);
+} else {
+	console.log("An element with class 'on-this-page-container' is required");
+}
