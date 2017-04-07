a72b8a40f3a3b13e44e3209159572ffcdf4897a7
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 07:57:02 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,15 @@
 var tags = require("./tags");
 
+/**
+ * @module {function} bit-docs-html-toc
+ * @parent plugins
+ *
+ * @description Injects a table of contents for the page into an HTML element.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs) {
 	var pkg = require("./package.json");
 
